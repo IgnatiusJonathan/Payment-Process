4947087e38a4d09ccf5e8744a635d39d3252116f
IgnatiusJonathan <ignatiusjonathantjandra@gmail.com>
2025-11-12 16:42:39 +0700

    Setup homescreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:payment_process/widgets/navbar.dart';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Read and parse JSON that contains the card list
+  }
+}

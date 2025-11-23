c8b089adf571e00184293c9a2aab3a8f012756a8
IgnatiusJonathan <ignatiusjonathantjandra@gmail.com>
2025-11-23 16:06:24 +0700

    Menambah welcome message ke dashboard/home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:payment_process/widgets/navbar.dart';
 
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Read and parse JSON that contains the card list
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body:SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                "Welcome to Payment Processor!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                "How can we help you?",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.black54,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 10,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        )
+      )
+    );
   }
 }
